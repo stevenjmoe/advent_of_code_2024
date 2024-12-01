c8d67fbf6ae8a0e2bbf38d98f7e4955ad6f4de34
Steven <dev@stevenm.co>
2024-12-01 22:47:03 +1100

    Day 1 problem 2

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,9 @@
 open Advent_of_code_2024.Day1
 
-let () =
-  let l = solution in
-  Printf.printf "%d" l;
+let day1 =
+  Printf.printf "Day 1 Prolem 1: %d\n" solution1;
+  Printf.printf "Day 1 Problem 2: %d\n" solution2;
   ()
 ;;
 
+let () = day1
--- lib/day1.ml
+++ lib/day1.ml
@@ -1,26 +1,28 @@
 let file = "input/day1.txt"
 
-let solution1 =
+let get_lists () =
   let split_location_ids line =
     let location_ids = line |> String.split_on_char ' ' in
     let id1 = location_ids |> List.hd |> int_of_string in
     let id2 = List.nth location_ids 3 |> int_of_string in
     id1, id2
   in
-  let list1, list2 =
-    File_reader.read_file file
-    |> List.fold_left
-         (fun (l1, l2) line ->
-           match l1, l2 with
-           | [], _ ->
-             let id1, id2 = split_location_ids line in
-             id1 :: [], id2 :: []
-           | rest1, rest2 ->
-             let id1, id2 = split_location_ids line in
-             id1 :: rest1, id2 :: rest2)
-         ([], [])
-  in
+  File_reader.read_file file
+  |> List.fold_left
+       (fun (l1, l2) line ->
+         match l1, l2 with
+         | [], _ ->
+           let id1, id2 = split_location_ids line in
+           id1 :: [], id2 :: []
+         | rest1, rest2 ->
+           let id1, id2 = split_location_ids line in
+           id1 :: rest1, id2 :: rest2)
+       ([], [])
+;;
+
+let solution1 =
   let sort a b = compare a b in
+  let list1, list2 = get_lists () in
   let sorted1 = list1 |> List.sort sort in
   let sorted2 = list2 |> List.sort sort in
   let _, result =
@@ -33,4 +35,22 @@ let solution1 =
          (0, 0)
   in
   result
+;;
+
+let solution2 =
+  let list1, list2 = get_lists () in
+  let list2_hash = Hashtbl.create 1000 in
+  List.iter (fun id -> Hashtbl.add list2_hash id 0) list2;
+  let list1_hash = Hashtbl.create 1000 in
+  List.iter
+    (fun id ->
+      let count = Hashtbl.find_all list2_hash id |> List.length in
+      Hashtbl.add list1_hash id count)
+    list1;
+  Hashtbl.fold
+    (fun k v acc ->
+      let r = k * v in
+      r + acc)
+    list1_hash
+    0
 ;;
